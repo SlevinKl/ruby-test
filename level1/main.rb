980ba01a264a375e0eabdf1a2757e91f533a8fcf
Rayane Moula <moula.rayane@gmail.com>
2023-06-19 10:59:22 +0200

    level 2

--- level1/main.rb
+++ level1/main.rb
@@ -1,16 +1,20 @@
 require 'json'
 
-data = JSON.parse(File.read('./level1/data.json'))
+data = JSON.parse(File.read('./level2/data.json'))
 
 data['workers'].each do |worker|
   number_of_shifts = data['shifts'].count { |shift| shift['user_id'] == worker['id'] }
-  worker['number_of_shifts'] = number_of_shifts
-  worker['pay'] = number_of_shifts * worker['price_per_shift']
+  price_per_shift = case worker['status']
+                    when 'medic' then 270
+                    when 'interne' then 126
+                    else worker['price_per_shift']
+                    end
+  worker.merge!('number_of_shifts' => number_of_shifts, 'pay' => price_per_shift * number_of_shifts)
 end
 
 output_data = { "workers": data['workers'] }
 
-File.open('./level1/output.json', 'w') do |file|
+File.open('./level2/output.json', 'w') do |file|
   file.write(JSON.pretty_generate(output_data))
 end
 
